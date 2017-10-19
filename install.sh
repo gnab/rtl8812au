4a643b502f5684f8b1fe2ae49d356cbfaacb6d8a
gmdutra <devgabrieldutra@gmail.com>
2017-10-19 11:32:16 -0200

    criando automatizador de instalacao do driver

--- install.sh
+++ install.sh
@@ -5,6 +5,23 @@ if [ "$(id -u)" != "0" ]; then
    exit 1
 fi
 
+apt install build-essential bc git wget
+
+cd /usr/src
+
+git clone --depth 1 https://github.com/raspberrypi/linux.git
+
+ln -s linux $(uname -r)
+ln -s /usr/src/linux /lib/modules/$(uname -r)/build
+cd linux
+
+wget -O Module.symvers https://raw.githubusercontent.com/raspberrypi/firmware/master/extra/Module7.symvers
+KERNEL=kernel7
+
+make bcm2709_defconfig
+make prepare
+make modules_prepare
+
 make &&
 cp 8812au.ko /lib/modules/$(uname -r)/kernel/drivers/net/wireless &&
 depmod &&
