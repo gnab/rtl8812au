3e30208aa196832db5da57d157cf4eb5271925eb
Vladimir Vassilev <vladimir@transpacket.com>
2019-07-16 14:01:19 +0100

    Update for 4.19 fixes .ndo_select_queue = rtw_select_queue incompatible pointer types

--- os_dep/linux/os_intfs.c
+++ os_dep/linux/os_intfs.c
@@ -1009,14 +1009,24 @@ unsigned int rtw_classify8021d(struct sk_buff *skb)
 	return dscp >> 5;
 }
 
+#if (LINUX_VERSION_CODE>=KERNEL_VERSION(4,19,0))
+static u16 rtw_select_queue(struct net_device *dev, struct sk_buff *skb,
+                            struct net_device *sb_dev,
+                            select_queue_fallback_t fallback)
+
+#else
 static u16 rtw_select_queue(struct net_device *dev, struct sk_buff *skb
+
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(3, 13, 0))
 			    , void *unused
 #endif
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(3, 14, 0))
 			    , select_queue_fallback_t fallback
 #endif
-){
+)
+#endif
+
+{
 	_adapter	*padapter = rtw_netdev_priv(dev);
 	struct mlme_priv *pmlmepriv = &padapter->mlmepriv;
 
